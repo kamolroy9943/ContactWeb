d09c491632cb91e89d51a98e7f3e405b5a8938e7
Kamol roy <kamolroy.din@gmail.com>
2017-04-08 21:17:24 +0600

    Initial Commit

---
+++ WebContact/WebContact/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebContact.Startup))]
+namespace WebContact
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
